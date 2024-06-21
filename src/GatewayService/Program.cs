0128d1d1c6d219f020710ee1fb6532e918e4f65d
Mark Rocke <markrockecoding@gmail.com>
2024-06-21 08:57:50 -0400

    Notification Service Added

--- src/GatewayService/Program.cs
+++ src/GatewayService/Program.cs
@@ -15,7 +15,20 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
 
 });
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("customPolicy", build =>
+    {
+        build.AllowAnyHeader()
+        .AllowAnyMethod()
+        .AllowCredentials()
+        .WithOrigins(builder.Configuration["ClientApp"]);
+    });
+});
+
 var app = builder.Build();
+
+app.UseCors();
 
 app.MapReverseProxy();
 
